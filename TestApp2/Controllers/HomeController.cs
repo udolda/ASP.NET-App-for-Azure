2c03084e747052d81b088be2c86581936da8886d
alkst <alkst017@gmail.com>
2020-12-20 23:18:44 +0300

    login and logout branch

--- TestApp2/Controllers/HomeController.cs
+++ TestApp2/Controllers/HomeController.cs
@@ -39,11 +39,13 @@ namespace TestApp2.Controllers
             ViewBag.ReturnUrl = returnUrl;
 
             var collectionUri = new Uri("https://dev.azure.com/LATeamInc/");
-            Session["token"] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJuYW1laWQiOiJkMGIyMjQ3My1jYjEyLTZmYTQtYTY0ZS00NGQwMWQ1NzlhNmQiLCJzY3AiOiJ2c28uY29kZSB2c28uaWRlbnRpdHlfbWFuYWdlIiwiYXVpIjoiNjVmMGRlM2MtYzUxNC00ZjBkLTg0ZDMtMjdjYTUwNTg2YTc2IiwiYXBwaWQiOiIyODVmMWJmNC02OWZhLTQ4N2EtYWY5YS1mN2Y1OTQ5NGQzYjMiLCJpc3MiOiJhcHAudnN0b2tlbi52aXN1YWxzdHVkaW8uY29tIiwiYXVkIjoiYXBwLnZzdG9rZW4udmlzdWFsc3R1ZGlvLmNvbSIsIm5iZiI6MTYwODQ4NjQ4NCwiZXhwIjoxNjA4NDkwMDg0fQ.zrntyRQw4VGOQFHbFtC_geZR4PedmI3o8JSEu0hJuMHHenic0voEFI_JRoM1ADq5k8Pre0fNCjpFWB4FNkMokS42xBdgRYT9ilpE0be8J94fCq-NBIa2xGYKlwuTSi9DGu9ZhL2tRpbqM5McTlgyNkCxry9J7iO81YeidrNh5Xl_368UPLx4mHvML_qgUVq60GkIY3Zn63E1q3QSeV_0azes6ZZeTQpoPVE4pPcM-nL3VurJkun2Ihl0g-nK1UDuTUnQ2OGLbPZZNNfLVRsUZRFNoqYOrhoqc4JlMJJjchR_LwmACnuHOEkhHvjQ3jHao-MsXuExEvjJ3ORKEGoosA";
+            Session["token"] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJuYW1laWQiOiI4YTRhNjk3My1mMGZiLTZiZDgtYjBjYS0wNjRiMDc2OWZmNzciLCJzY3AiOiJ2c28uY29kZSB2c28uaWRlbnRpdHlfbWFuYWdlIiwiYXVpIjoiMDRjYWQyZWMtYTgzYy00NjkwLTk2ZjAtZmFhNzgwMWZmMWVmIiwiYXBwaWQiOiIyODVmMWJmNC02OWZhLTQ4N2EtYWY5YS1mN2Y1OTQ5NGQzYjMiLCJpc3MiOiJhcHAudnN0b2tlbi52aXN1YWxzdHVkaW8uY29tIiwiYXVkIjoiYXBwLnZzdG9rZW4udmlzdWFsc3R1ZGlvLmNvbSIsIm5iZiI6MTYwODQ5NDYzNywiZXhwIjoxNjA4NDk4MjM3fQ.iU8IMFyjEzVoDjGLj-fv1iVB-ZUUf7fi5mgY2SGyq3BrtAcvcrNc9L0sWU4lvBGi5ySFJIt4DDo_p-pIfYYi-9EtNGX6Ef5kmrd4XPX9Cny8VOMLSNemLB1Uj_56IzEeylUMSatOo_1hWDCBJUv5-fU4TDDuco-mFUrS2YqhuGiBAqhgLguHMCxDdNvD8X6WjMyrjXOOi5ea29KWTqzZeCqv1iLW4etciRdGL3vlRAzPMzJmkUS50lxc1lUPoXuPxr_QEsBH5piSoEsliIHO-gT8elCnzb8gpKEJMw4fUPVUQj2YZaMH99ZM7LX0dKlyT3IgFn_tmF_LXr5_0eablw";
             VssConnection connection = new VssConnection(new Uri("https://dev.azure.com/LATeamInc/"), new VssOAuthAccessTokenCredential((string)Session["token"]));
             Session["connect"] = connection;
             string s = connection.AuthorizedIdentity.DisplayName;
             bool t = connection.HasAuthenticated;
+
+            
             //GetInfo.SampleREST(connection, "WorkPractice");
             //var auth = new Authenticate();
             //var teamPC = (TfsTeamProjectCollection)Session["tfs"];
